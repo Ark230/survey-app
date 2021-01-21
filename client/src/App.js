2edf12c3abc13faa90f4222b2ca8be02fdb7b18c
Ignacio Pérez Cumillaf <iperezc99@gmail.com>
2021-01-21 11:03:20 -0300

    setting up project files

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Login from './pages/login/login.component';
+
+
+const App = () => {
+
+    return(
+        <div>
+            <Login/>
+        </div>
+    );
+
+
+
+}
+
+export default App;
